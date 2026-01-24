67ec94d15c8e860c8ec34cca5326b1afcf166c6e
Junbo Qiao <77002546+Junboooo@users.noreply.github.com>
2026-01-24 18:04:40 +0800

    Update example.py

--- src/scripts/example.py
+++ src/scripts/example.py
@@ -5,8 +5,8 @@ export LOG_PATH="./debug_log_2b_GRPO_coco_base65cate_6k.txt"
 # export LD_LIBRARY_PATH=/usr/local/cuda-12.1/compat:$LD_LIBRARY_PATH
 # export CUDA_HOME=/usr/local/cuda
 
-export DATA_PATH=/work/docker/GRPO/grpo-lumina/src/virft/src/open_r1/test500_v2
-export CKPT_PATH=/work/docker/GRPO/grpo-lumina/share_models/RealSR-R1_lr5e6/checkpoint-30 #/work/docker/GRPO/PURE-main/pure/ckpts/Alpha-VLLM/PRUE #/work//docker/GRPO/grpo-lumina/Qwen2-VL-2B-Instruct # 
+export DATA_PATH=/work/docker/GRPO/grpo-lumina/src/realsr-r1/src/open_r1/test500_v2
+export CKPT_PATH=/work/docker/GRPO/grpo-lumina/share_models/RealSR-R1_lr5e6/checkpoint-30 
 export SAVE_PATH=./share_models/RealSR-R1_lr2e6
 
 export PYTHONPATH=$PYTHONPATH:/work//docker/GRPO/grpo-lumina/src/realsr-r1/src
@@ -18,11 +18,11 @@ torchrun --nproc_per_node="6" \
     --node_rank="0" \
     --master_addr="127.0.0.1" \
     --master_port="11317" \
-    src/virft/src/open_r1/grpo.py \
+    src/realsr-r1/src/open_r1/grpo.py \
     --output_dir ${SAVE_PATH}  \
     --model_name_or_path ${CKPT_PATH} \
     --dataset_name ${DATA_PATH} \
-    --deepspeed /work/docker/GRPO/Visual-RFT-main/src/virft/local_scripts/zero3.json \
+    --deepspeed /work/docker/GRPO/Visual-RFT-main/src/realsr-r1/local_scripts/zero3.json \
     --max_prompt_length 5000 \
     --max_completion_length 7000 \
     --per_device_train_batch_size 1 \
